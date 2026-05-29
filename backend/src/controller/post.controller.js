9248f3ad93488b20e6c81172920706c480bf7794
Kshitij Himanshu <kshitij7320@gmail.com>
2026-05-29 23:23:00 +0530

    added upload feature

--- backend/src/controller/post.controller.js
+++ backend/src/controller/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model")
 const ImageKit  = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
 
 const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,           
@@ -11,12 +12,34 @@ const imagekit = new ImageKit({
 async function createPostController(req, res){
     console.log(req.body, req.file)
 
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"})
+    }
+
+    let decoded= null
+
+try {
+     decoded = jwt.verify(token, process.env.JWT_SECRET)
+} catch (error) {
+    return res.status(401).json({message:"Invalid token"})
+}
+    
+
     const file = await imagekit.files.upload({
         file:await toFile(Buffer.from(req.file.buffer),"file"),
         fileName: req.file.originalname,
         folder: "posts"
     })
-    res.send(file)
+
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({message:"Post created successfully", post})
 }
 
 module.exports = {
