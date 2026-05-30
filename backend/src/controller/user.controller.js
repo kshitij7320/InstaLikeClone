66c03a745a9ec6f7627335fb6f9d4192b1903771
Kshitij Himanshu <kshitij7320@gmail.com>
2026-05-31 01:45:45 +0530

    initial db optimization

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -5,6 +5,34 @@ async function followUserController(req,res){
 const followerUsername = req.user.username;
 const followeeUsername = req.params.username;
 
+if(followeeUsername == followerUsername){
+ return res.status(400).json({
+    message:"You cannot follow yourself"
+ })
+}
+
+const isFolloweeExists = await userModel.findOne({
+    username: followeeUsername
+})
+
+if(!isFolloweeExists){
+    return res.status(404).json({
+        message:"User not exists"
+    })
+}
+
+const isAlreadyFollowing = await followModel.findOne({
+    followee: followeeUsername,
+    follower: followerUsername
+})
+
+if(isAlreadyFollowing){
+    return res.status(200).json({
+        message:`You are already following ${followeeUsername}`,
+        follow: isAlreadyFollowing
+    })
+}
+
 const followRecord = await followModel.create({
     follower: followerUsername,
     followee: followeeUsername
