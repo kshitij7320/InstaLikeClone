7f800aec8d7c41cdf9bf66026da71bbe364a679d
Kshitij Himanshu <kshitij7320@gmail.com>
2026-05-31 13:47:00 +0530

    added status

--- backend/src/models/follow.model.js
+++ backend/src/models/follow.model.js
@@ -8,6 +8,14 @@ const followSchema = new mongoose.Schema({
     },
     followee: {
         type: String
+    },
+    status:{
+        type: String,
+        default: "pending",
+        enum:{
+            values:["pending","accepted","rejected"],
+            message: "Status can only be pending,accepted or rejected"
+        }
     }
 },{timestamps: true})
 
