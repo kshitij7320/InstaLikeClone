fa4d3b8eba581e0cfa2ec4d7e1d2c2999af709cb
Kshitij Himanshu <kshitij7320@gmail.com>
2026-06-02 23:44:45 +0530

    added:api in js file

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,12 +1,40 @@
-async function register(username, email, password) {
+import axios from "axios";
 
+
+export async function register(username, email, password) {
+    try{
+        const response = await axios.post("http://localhost:3000/api/auth/register",{
+            username,
+            email,
+            password
+        },{
+            withCredentials: true
+        })
+        return response.data
+    }
+    catch(err){
+        console.log(err)
+        throw err
+    }
 
 
 }
 
 
-async function login(email, password) {
+export async function login(email, password) {
+    try{
+        const response = await axios.post("http://localhost:3000/api/auth/login",{
+            email,
+            password
+        },{
+            withCredentials: true
+        })
+        return response.data
+    }
+    catch(err){
+        console.log(err)
+        throw err
+    }
 
 
-    
 }
